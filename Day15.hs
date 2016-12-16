0f3a311ea1d0730c44b448b2f123085bb86ac2ba
Tristan Seligmann <mithrandi@mithrandi.net>
2016-12-16 18:29:09 +0200

    Day 15.

---
+++ Day15.hs
@@ -0,0 +1,43 @@
+module Main where
+
+import Data.List
+
+data Disc = Disc
+  { initial :: Int
+  , size :: Int
+  }
+
+positions :: [Disc] -> [[Int]]
+positions = map positions'
+  where positions' disc = map (\i -> (initial disc + i) `mod` size disc) [0..]
+
+discs :: [Disc]
+discs =
+  [ Disc 1 17
+  , Disc 0 7
+  , Disc 2 19
+  , Disc 0 5
+  , Disc 0 3
+  , Disc 5 13
+  ]
+
+discs2 :: [Disc]
+discs2 = discs ++ [Disc 0 11]
+
+example :: [Disc]
+example =
+  [ Disc 4 5
+  , Disc 1 2
+  ]
+
+machine :: [[Int]] -> Bool
+machine = machine' 1
+  where machine' time' (p:ps) = p !! time' == 0 && machine' (time' + 1) ps
+        machine' _ [] = True
+
+main :: IO ()
+main = do
+  print $ machine (map (drop 5) (positions example))
+  print . findIndex machine $ iterate (map tail) (positions example)
+  print . findIndex machine $ iterate (map tail) (positions discs)
+  print . findIndex machine $ iterate (map tail) (positions discs2)
